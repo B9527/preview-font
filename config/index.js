940939c06473728471d5fdb88e1bfde5aa0e8d32
baiyang <1335239218@qq.com>
2018-01-11 23:46:49 +0800

    baiyang

--- config/index.js
+++ config/index.js
@@ -10,10 +10,18 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://192.168.88.132:8000',
+        changeOrigin: true,
+        pathRewrite:function(path, req){
+          return path.replace('/api', 'preview')
+        }
+      }
+    },
 
     // Various Dev Server settings
-    host: '10.10.30.70', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
--- src/views/api/api.js
+++ src/views/api/api.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
-let base = 'http://10.10.30.70:8000/preview';
 let config = {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   }
+let config_2 = {
+  headers:{
+    "Access-Control-Allow-Credentials": "true",
+    "withCredentials ":"true"
+  }
+}
 
-  export const requestLogin = params => { return axios.post(`${base}/login/`, params).then(res => res.data); };
-  export const setUpload = params => { return axios.post(`${base}/config/upload/`, params, config).then(res => res.data); };
-  export const setInput = params => { return axios.post(`${base}/config/input/`, params).then(res => res.data); };
-  export const getConfigList = params =>{ return axios.get(`${base}/config/list/`, params).then(res => res.data); };
-  export const getTableInfo = params => {  return axios.get(`${base}/table/`, params).then(res => res.data); };
-  export const getTableData = params => { return axios.get(`${base}/table/data/`, params).then(res => res.data); };
+  export const requestLogin = params => { return axios.post(`api/login/`, params).then(res => res.data); };
+  export const setUpload = params => { return axios.post(`api/config/upload/`, params, config).then(res => res.data); };
+  export const setInput = params => { return axios.post(`api/config/input/`, params).then(res => res.data); };
+  export const getConfigList = params =>{ return axios.get(`api/config/list/`, params).then(res => res.data); };
+  export const getTableInfo = params => {  return axios.get(`api/table/`, params).then(res => res.data); };
+  export const getTableData = params => { return axios.get(`api/table/data/`, params).then(res => res.data); };
